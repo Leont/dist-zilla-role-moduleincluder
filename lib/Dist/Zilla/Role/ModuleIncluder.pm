1e5d95ee22914f2c26660e5bca3989692cfe4966
Karen Etheridge <ether@cpan.org>
2014-12-23 14:15:24 -0800

    add some extra logging for helping to debug strange situations

--- lib/Dist/Zilla/Role/ModuleIncluder.pm
+++ lib/Dist/Zilla/Role/ModuleIncluder.pm
@@ -29,9 +29,11 @@ sub _should_skip {
 }
 
 sub _get_reqs {
-	my ($reqs, $scanner, $module, $background, $blacklist) = @_;
+	my ($self, $reqs, $scanner, $module, $background, $blacklist) = @_;
 	my $module_file = Module::Metadata->find_module_by_name($module) or confess "Could not find module $module";
 	my %new_reqs = %{ $scanner->scan_file($module_file)->as_string_hash };
+	$self->log_debug([ 'found dependency of %s: %s %s', $module, $_, %new_reqs{$_} ]) foreach keys %new_reqs;
+
 	my @real_reqs = grep { !_should_skip($_, $new_reqs{$_}, $blacklist, $background) } keys %new_reqs;
 	for my $req (@real_reqs) {
 		if (defined $reqs->{$module}) {
@@ -40,8 +42,9 @@ sub _get_reqs {
 		}
 		else {
 			$reqs->{$req} = $new_reqs{$req};
-			_get_reqs($reqs, $scanner, $req, $background, $blacklist);
+			$self->_get_reqs($reqs, $scanner, $req, $background, $blacklist);
 		}
+		$self->log_debug([ 'adding to requirements list: %s %s', $req, $new_reqs{$req} ]);
 	}
 	return;
 }
@@ -57,10 +60,11 @@ sub include_modules {
 	my %reqs;
 	my $scanner = Perl::PrereqScanner->new;
 	my %blacklist = map { ( $_ => 1 ) } 'perl', @{ $options->{blacklist} || [] };
-	_get_reqs(\%reqs, $scanner, $_, $version->{ _version_normalize($background) }, \%blacklist) for keys %modules;
+	$self->_get_reqs(\%reqs, $scanner, $_, $version->{ _version_normalize($background) }, \%blacklist) for keys %modules;
 	my @modules = grep { !$modules{$_} } keys %modules;
 	my %location_for = map { _mod_to_filename($_) => Module::Metadata->find_module_by_name($_) } uniq(@modules, keys %reqs);
 	for my $filename (keys %location_for) {
+		$self->log_debug([ 'copying for inclusion: %s', $location_for{$filename} ]);
 		my $file = Dist::Zilla::File::InMemory->new({name => $filename, encoded_content => read_binary($location_for{$filename})});
 		$self->add_file($file);
 	}
